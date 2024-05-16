9a11e62b4e7d3a9053182e5c7360f3ded97b16ee
Jack Palmer <165166334+4553m81y@users.noreply.github.com>
2024-05-16 17:13:27 +1000

    Create example.s

---
+++ example.s
@@ -0,0 +1,17 @@
+global _start
+
+section .data
+    message db "Hello HTB Academy!"
+    length equ $-message
+
+section .text
+_start:
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, message
+    mov rdx, length
+    syscall
+
+    mov rax, 60
+    mov rdi, 0
+    syscall
