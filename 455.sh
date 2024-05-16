336f13a2634aab3f3cb7e1c402e1611cde4e82e2
Jack Palmer <165166334+4553m81y@users.noreply.github.com>
2024-05-16 17:06:21 +1000

    Update 455.sh

--- 455.sh
+++ 455.sh
@@ -1,7 +1,53 @@
 #!/bin/bash
 
-fileName="${1%%.*}" # remove .s extension
+# Function to display usage
+usage() {
+    echo "Usage: $0 <assembly-file> [--arch <architecture>]"
+    echo "Supported architectures: x86, arm"
+    exit 1
+}
 
-nasm -f elf64 ${fileName}".s"
-ld ${fileName}".o" -o ${fileName}
-[ "$2" == "-g" ] && gdb -q ${fileName} || ./${fileName}
+# Default architecture
+arch="x86"
+
+# Parse arguments
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        --arch)
+            arch="$2"
+            shift 2
+            ;;
+        *)
+            if [[ -z "$file" ]]; then
+                file="$1"
+            else
+                usage
+            fi
+            shift
+            ;;
+    esac
+done
+
+# Check if file is provided and has an acceptable extension
+if [[ -z "$file" || ! "$file" =~ \.(s|asm|S|a|as|asm64)$ ]]; then
+    usage
+fi
+
+fileName="${file%%.*}" # remove the extension
+
+case $arch in
+    x86)
+        nasm -f elf64 "${fileName}.${file##*.}"
+        ld "${fileName}.o" -o "${fileName}"
+        ;;
+    arm)
+        as -o "${fileName}.o" "${fileName}.${file##*.}"
+        ld -o "${fileName}" "${fileName}.o"
+        ;;
+    *)
+        echo "Unsupported architecture: $arch"
+        usage
+        ;;
+esac
+
+./"${fileName}"
